6528b0beb789e729ec398ea9104a15281d1d62ce
Ufuk Yucel <ufukyucel@entremax.media>
2026-08-19 01:09:06 +0300

    Initial Commit

---
+++ RevivalControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RevivalControl
+//
+//  Created by yuceloper on 19.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RevivalControl/RevivalControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevivalControlApp.swift
+//  RevivalControl
+//
+//  Created by yuceloper on 19.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct RevivalControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
